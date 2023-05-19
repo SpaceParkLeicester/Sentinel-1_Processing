a505cb14925d18657f79b1b4ae0a4f7ee02a3413
vrym2 <vardhan609@gmail.com>
2023-05-19 11:23:26 +0000

    grd process with pyrosar

--- src/pyro_sar/grd_process.py
+++ src/pyro_sar/grd_process.py
@@ -4,64 +4,140 @@ from pyroSAR.auxdata import dem_autoload
 from spatialist import Vector
 from pyroSAR.snap import geocode, gpt
 
+import os
+import shutil
 import geopandas as gpd
 from Sentinel_SAR.src.data import OilTerminals
 
 import logging
-# basic info with some message filtering
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 class sar_processing:
-    terminals = OilTerminals()
-    data_dict = terminals.read_data()
-    data_dict = terminals.wkt_polygon()
+    """SAR data processing"""
 
-    filename = 'flotta'
-    poly = data_dict["flotta"]
-
+    @staticmethod
+    def remove_files(folder_path:str = None)-> None:
+        """Remove all files in a folder"""
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        else:
+            for file in os.listdir(folder_path):
+                if os.path.isfile(os.path.join(folder_path, file)):
+                    os.remove(os.path.join(folder_path, file))
+                else:
+                    shutil.rmtree(os.path.join(folder_path, file))
+     
     def __init__(
             self,
-            sar_zip_file:str = None) -> None:
+            sar_zip_file:str = None,
+            location_name:str = None) -> None:
         self.sar_zip_file = sar_zip_file
+        self.location_name = location_name
+    
+    def location_wkt(self)-> None:
+        """Getting the Polygon of oil terminal location"""
+        terminals = OilTerminals()
+        data_dict = terminals.read_data()
+        data_dict = terminals.wkt_polygon()
+        self.poly = data_dict[self.location_name]
 
-    def naming(self)->None:
+    def naming(self):
+        """Identifying SAR file, if printed gives description"""
         self.id = identify(self.sar_zip_file)
+        return self.id
     
     def get_orbit_file(self)-> None:
-        self.orbit_path = self.id.getOSV(osvdir='/home/vardh/.snap/auxdata/Orbits/Sentinel-1/', osvType='POE', returnMatch=True)
+        """Getting the Orbit file"""
+        osvdir = '.snap/auxdata/Orbits/Sentinel-1/'
+        osvdir = os.path.join(os.path.expanduser('~'), osvdir)
+        try:
+            os.path.exists(osvdir)
+            self.orbit_path = self.id.getOSV(osvdir = osvdir, osvType='POE', returnMatch=True)
+        except Exception:
+            logging.debug("Make sure to install ESA SNAP")
+            logging.debug("https://step.esa.int/main/download/snap-download/")
     
-    def dem_file(self)-> None: 
+    def dem_file(self)-> None:
+        """Auto-loading DEM file"""
+        # Creating SHape file
         gdf = gpd.GeoDataFrame(geometry=[self.poly])
         gdf.crs = 'EPSG:4326'
-        output_file = 'data/shp/flotta/flotta.shp'
-        gdf.to_file(output_file, driver='ESRI Shapefile')
+        shp_dir = f'data/shp/{self.location_name}'
 
-        with Vector(output_file) as vec:
+        self.remove_files(shp_dir)
+        
+        shp_file = os.path.join(shp_dir, f'{self.location_name}.shp')
+        gdf.to_file(shp_file, driver='ESRI Shapefile')
+
+        # DEM auto loading
+        with Vector(shp_file) as vec:
             vrt = dem_autoload(geometries=[vec],
                             demType='SRTM 1Sec HGT',
                             buffer=0.1)
-
+            
+    def pol_stamp(self)-> None:
+        """Getting the polarisation of SAR file"""
+        self.file_name = os.path.basename(self.sar_zip_file)
+        self.uuid = self.file_name.split('.')[0]
+        self.polstamp = self.uuid.split("_")[3]
+        self.polarization = self.polstamp[2:4]         
+        print(self.polarization)
+        if self.polarization == 'DV':
+            self.pols = ['VH','VV']
+        elif self.polarization == 'DH':
+            self.pols = ['HH','HV']
+        elif self.polarization == 'SH' or self.polarization == 'HH':
+            self.pols = ['HH']
+        elif self.polarization == 'SV':
+            self.pols = ['VV']
+        else:
+            self.pols = None
+            logging.error("Polarization error!")
+        
+        if not self.pols is None:
+            logging.info(f"Product {self.uuid} is of polarisation {self.pols}")
+        return self.pols
+    
     def snap_process(
             self,
-            out_dir:str = None,
-            shp_file:str = None)-> None:
-        geocode(
-            infile = self.sar_zip_file,
-            outdir = out_dir,
-            shapefile = shp_file,
-            polarizations = 'VV',
-            imgResamplingMethod = 'BILINEAR_INTERPOLATION',
-            speckleFilter = 'Refined Lee',
-            refarea = 'sigma0',
-            returnWF = True)
+            out_dir:str = 'data/processed',
+            shp_file:str = 'data/shp')-> None:
+        """SAR data pre-processing with pyoSAR"""
+        # Setting the output Directory for processed files
+        out_dir = os.path.join(out_dir, self.location_name)
+        self.remove_files(out_dir)
+
+        # Getting the Shape files
+        shp_file = os.path.join(shp_file, self.location_name)
+        shp_file = os.path.join(shp_file, f'{self.location_name}.shp')
+
+        # Performing the default SAR processing from PyroSAR python package
+        # https://pyrosar.readthedocs.io/en/latest/index.html
+        if self.pols is not None:
+            for pol in self.pols:
+                logging.info(f"SAR-Processing for {self.uuid}, polarisation - {pol}")
+                geocode(
+                    infile = self.sar_zip_file,
+                    outdir = out_dir,
+                    shapefile = shp_file,
+                    polarizations = pol,
+                    imgResamplingMethod = 'BILINEAR_INTERPOLATION',
+                    speckleFilter = 'Refined Lee',
+                    refarea = 'sigma0',
+                    returnWF = True)
+                logging.info("SAR-Processing finished!")
         
 if __name__ == "__main__":
-    sar_zip_file = '/home/vardh/apps/tmp/S1A_IW_GRDH_1SDV_20230313T175210_20230313T175235_047629_05B868_4E5C.zip'
-    out_dir = 'data/pre_process/graphs'
-    shp_file = 'data/shp/flotta/flotta.shp'
-    sar = sar_processing(sar_zip_file)
+    sar_zip_file = 'downloads/S1_data/S1A_IW_GRDH_1SDV_20230514T062300_20230514T062325_048526_05D642_A40C.zip'
+    sar_zip_file = os.path.join(os.path.expanduser('~'), sar_zip_file)
+    location_name = 'stanlow'
+
+    sar = sar_processing(
+        sar_zip_file,
+        location_name)
+    sar.location_wkt()
     sar.naming()
     sar.get_orbit_file()
     sar.dem_file()
-    sar.snap_process(
-        out_dir = out_dir)
+    sar.pol_stamp()
+    sar.snap_process()
